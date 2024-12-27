8e14453164eddebd1bb0d8df9f75bfef8fd70be7
antonio mauricio <antonioimportant@gmail.com>
2024-12-27 11:09:33 -0300

    feat: renames accentColor to gradientColor + adds gradients to all discs

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div
       style={{
-        background: discs[currentTrackIndex].accentColor,
+        background: discs[currentTrackIndex].gradientColor,
       }}
     >
       <DiscInfo disc={discs[currentTrackIndex]} />
