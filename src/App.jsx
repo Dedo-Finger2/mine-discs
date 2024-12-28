0ee76ccf92eb8859b072afe85f56792a97664ce7
antonio mauricio <antonioimportant@gmail.com>
2024-12-28 10:14:54 -0300

    feat: adds a vinhete

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,38 @@ import { useState } from "react";
 import DiscInfo from "./components/DiscInfo";
 import MusicPlayer from "./components/MusicPlayer";
 import discs from "./database";
+import { useEffect } from "react";
 
 function App() {
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
 
+  useEffect(() => {
+    document.body.style.background = discs[currentTrackIndex].gradientColor;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.height = "100vh";
+
+    return () => {
+      document.body.style.background = "";
+    };
+  }, [currentTrackIndex]);
+
   return (
-    <div
-      style={{
-        background: discs[currentTrackIndex].gradientColor,
-      }}
-    >
+    <div>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+          background:
+            "radial-gradient(circle, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.7) 100%)",
+          pointerEvents: "none", // Para garantir que não interfira nos cliques
+        }}
+      />
       <DiscInfo disc={discs[currentTrackIndex]} />
       <MusicPlayer
         audioSrc={discs[currentTrackIndex].trackPath}
